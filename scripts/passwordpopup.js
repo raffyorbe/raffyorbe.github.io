8f622138e1d0bbf2a5c7080d05958778cb02a1c0
raffyorbe <8110851+raffyorbe@users.noreply.github.com>
2026-03-13 20:04:44 -0400

    Delete scripts/passwordpopup.js

--- scripts/passwordpopup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const showPopupButton = document.getElementById("showPopup");
-    const popupModal = new bootstrap.Modal(document.getElementById("popupModal"));
-
-    showPopupButton.addEventListener("click", function () {
-      popupModal.show();
-    });
-  });
